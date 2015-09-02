a3bfd8cc490ee480648042d729151151fa84ce03
David Deizepi <deizepi@users.noreply.github.com>
2015-09-02 10:08:48 -0300

    Criacao da classe Empresa
    Nela ficarão armazenadas as informações sobre a empresa em relação à Cielo (número de filiação e chave de acesso)

---
+++ Empresa.php
@@ -0,0 +1,21 @@
+<?php
+
+class Empresa {
+
+	/**
+	 * Número de afiliação da loja com a Cielo.
+	 * @var integer NUMERO
+	 */
+    const NUMERO = "1006993069";
+
+	/**
+	 * Chave de acesso da loja atribuída pela Cielo
+	 * @var string CHAVE
+	 */
+    const CHAVE  = "25fbb99741c739dd84d7b06ec78c9bac718838630f30b112d033ce2e621b34f3";
+
+    function __construct(){
+
+    }
+
+}
